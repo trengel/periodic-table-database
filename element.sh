ff33a82b0350bd26108951da2604a49ecb99f74e
Tristan Engel <121832013+trengel@users.noreply.github.com>
2023-05-03 13:02:21 -0400

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo Please provide an element as an argument.
+case "${1}" in
+  1|H|Hydrogen) exit;;
+  2|He|Helium) exit;;
+  3|Li|Lithium) exit;;
+  4|Be|Beryllium) exit;;
+  5|B|Boron) exit;;
+  6|C|Carbon) exit;;
+  7|N|Nitrogen) exit;;
+  8|O|Oxygen) exit;;
+  9|F|Fluorine) exit;;
+  10|Ne|Neon) exit;;
+  '') echo Please provide an element as an argument.;;
+  *) echo I could not find that element in the database.;;
+esac
